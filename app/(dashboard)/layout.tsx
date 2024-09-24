32deb943d02e35fe7b023dba0fa7c46f262849e5
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-09-24 15:58:25 -0400

    esconder search input

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -119,7 +119,7 @@ function MobileNav() {
           </Link>
           <Link
             href="/alunos?status=ativo"
-            className="flex items-center gap-4 px-2.5 text-foreground"
+            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
           >
             <Users2 className="h-5 w-5" />
             Alunos
--- app/(dashboard)/search.tsx
+++ app/(dashboard)/search.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useTransition } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams, useSelectedLayoutSegments } from 'next/navigation';
 import { Input } from '@/components/ui/input';
 import { Spinner } from '@/components/icons';
 import { Search } from 'lucide-react';
@@ -13,11 +13,12 @@ export function SearchInput() {
   const status = searchParams.get('status');
   const offset = Number(searchParams.get('offset')) ?? 0;
   const q = searchParams.get('q') || '';
+  const segments = useSelectedLayoutSegments();
 
   function searchAction(formData: FormData) {
     const q = formData.get('q') as string;
     const newOffset = Math.max(0, offset - 12);
-    
+
     const queryString = `?offset=${newOffset}${q ? `&q=${q}` : ''}${searchParams.get('status') ? `&status=${status}` : ''}`;
 
     startTransition(() => {
@@ -25,18 +26,25 @@ export function SearchInput() {
     });
   }
 
-
   return (
     <form action={searchAction} className="relative ml-auto flex-1 md:grow-0">
-      <Search className="absolute left-2.5 top-[.75rem] h-4 w-4 text-muted-foreground" />
-      <Input
-        name="q"
-        type="search"
-        placeholder="Pesquisa..."
-        defaultValue={q}
-        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-      />
-      {isPending && <Spinner />}
+      {
+        segments[0] === 'conta' || segments[0] === 'configuracoes' ? (
+          <div></div>
+        ) : (
+          <>
+            <Search className="absolute left-2.5 top-[.75rem] h-4 w-4 text-muted-foreground" />
+            <Input
+              name="q"
+              type="search"
+              placeholder="Pesquisa..."
+              defaultValue={q}
+              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+            />
+            {isPending && <Spinner />}
+          </>
+        )
+      }
     </form>
   );
 }
